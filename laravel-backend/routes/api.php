9e07164dabb743ed3d64d0e0c328ae9ec6532071
Marcell145 <Marcellleeviroy9@gmail.com>
2025-12-15 10:42:49 +0700

    SuratController.php,api.php,MahasiswaController.php,PengajuanSurat.php,JenisSurat.php,index.vue,api.js

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,14 +1,41 @@
 <?php
 
-use App\Http\Controllers\Api\JadwalController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\JadwalController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\SuratController;
+use App\Http\Controllers\Api\MahasiswaController;
 
-Route::apiResource('/Jadwal', JadwalController::class)->except(['index','stream', 'destroy']);
+// JADWAL (dirapikan)
+Route::apiResource('/jadwal', JadwalController::class)->except(['index', 'destroy']);
 Route::get('/stream/jadwal', [JadwalController::class, 'stream']);
-Route::apiResource('/user', UserController::class)->except(['show']);
 
-// buat route khusus login & register
-Route::post('/login', [UserController::class, 'login']);     // login
-Route::post('/register', [UserController::class, 'store']);    // register
+// USER
+Route::apiResource('/user', UserController::class)->except(['show']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'store']);
+
+// =========================
+// SURAT
+// =========================
+
+// jenis surat
+Route::get('/jenis-surat', [SuratController::class, 'indexJenisSurat']);
+Route::post('/jenis-surat', [SuratController::class, 'storeJenisSurat']);
+Route::get('/jenis-surat/{id}', [SuratController::class, 'showJenisSurat']);
+Route::put('/jenis-surat/{id}', [SuratController::class, 'updateJenisSurat']);
+Route::delete('/jenis-surat/{id}', [SuratController::class, 'destroyJenisSurat']);
+
+// pengajuan surat
+Route::get('/pengajuan-surat', [SuratController::class, 'indexPengajuanSurat']);
+Route::post('/pengajuan-surat', [SuratController::class, 'storePengajuanSurat']);
+Route::get('/pengajuan-surat/{id}', [SuratController::class, 'showPengajuanSurat']);
+Route::put('/pengajuan-surat/{id}', [SuratController::class, 'updatePengajuanSurat']);
+Route::delete('/pengajuan-surat/{id}', [SuratController::class, 'destroyPengajuanSurat']);
+
+Route::patch('/pengajuan-surat/{id}/approve', [SuratController::class, 'approve']);
+Route::patch('/pengajuan-surat/{id}/reject', [SuratController::class, 'reject']);
+
+// MAHASISWA
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
